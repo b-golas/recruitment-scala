af0f0aaab7fc7a8bb6d13fb8da0f7059fca41c4b
Bartosz Gołas <bartoszp.golas@gmail.com>
2023-02-27 23:54:57 +0100

    make it simpler

--- src/main/scala/com/virtuslab/DataParser.scala
+++ src/main/scala/com/virtuslab/DataParser.scala
@@ -8,23 +8,21 @@ object DataParser {
 
   def parseData(input: RDD[String])(implicit spark: SparkSession): Dataset[IntKeyValuePair] = {
     import spark.implicits._
-    parseTextData(normaliseDelimiter(input))
-      .toDF("key", "value")
-      .as[IntKeyValuePair]
+    parseTextData(normaliseDelimiter(input)).toDS
   }
 
   def normaliseDelimiter(input: RDD[String]): RDD[String] =
     input
       .map(_.replace(",", "\t"))
 
-  private[virtuslab] def parseTextData(input: RDD[String]): RDD[(Int, Int)] =
+  private[virtuslab] def parseTextData(input: RDD[String]): RDD[IntKeyValuePair] =
     input
       .map(text => {
         text.split("\\t") match {
-          case Array(first) => (first.toInt, 0)
-          case Array(first, second) if first.isEmpty => (0, second.toInt)
-          case Array(first, second) => (first.toInt, second.toInt)
-          case Array() => (0, 0)
+          case Array(first) => IntKeyValuePair(first.toInt, 0)
+          case Array(first, second) if first.isEmpty => IntKeyValuePair(0, second.toInt)
+          case Array(first, second) => IntKeyValuePair(first.toInt, second.toInt)
+          case Array() => IntKeyValuePair(0, 0)
         }
       })
 
--- src/test/scala/com/virtuslab/DataParserTest.scala
+++ src/test/scala/com/virtuslab/DataParserTest.scala
@@ -1,19 +1,18 @@
 package com.virtuslab
 
 import com.virtuslab.DataParser.{normaliseDelimiter, parseTextData}
+import com.virtuslab.model.IntKeyValuePair
 import com.virtuslab.test.SparkTestSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class DataParserTest extends SparkTestSpec {
 
-  import spark.implicits._
-
   it should "normalise a delimiter" in {
-    val input = Seq(
+    val input = spark.sparkContext.parallelize(Seq(
       "1,1",
       "2\t2",
       "3,3"
-    ).toDF().as[String].rdd
+    ))
 
     normaliseDelimiter(input).collect().toSeq should contain theSameElementsAs Seq(
       "1\t1",
@@ -23,20 +22,20 @@ class DataParserTest extends SparkTestSpec {
   }
 
   it should "parse text data" in {
-    val input = Seq(
+    val input = spark.sparkContext.parallelize(Seq(
       "1\t1",
       "4\t4",
       "5\t",
       "\t5",
       "\t"
-    ).toDF().as[String].rdd
+    ))
 
     parseTextData(input).collect().toSeq should contain theSameElementsAs Seq(
-      (1, 1),
-      (4, 4),
-      (5, 0),
-      (0, 5),
-      (0, 0)
+      IntKeyValuePair(1, 1),
+      IntKeyValuePair(4, 4),
+      IntKeyValuePair(5, 0),
+      IntKeyValuePair(0, 5),
+      IntKeyValuePair(0, 0)
     )
   }
 
